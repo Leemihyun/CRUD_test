a77821b4ed9f55df0463bf51f4ea2a762582dda5
leemihyun <m.lee@cosmocomputing.co.jp>
2023-09-02 21:54:19 +0900

    환경세팅 및 리액트쿼리 초기설정

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactQueryWrapper from "./Provider";
+
+const App = () => {
+    return (
+        <ReactQueryWrapper>
+            <div>
+                hello
+            </div>
+        </ReactQueryWrapper>
+    );
+};
+
+export default App;
